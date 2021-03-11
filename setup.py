55f7862febda7ca05726f675394ecb2804832f41
andrew hah <ahah@figure.com>
2021-03-11 14:57:28 +0000

    first

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cac_calibrate",
+    version="0.01",
+    description="",
+    author="Andrew Hah",
+    author_email="hahdawg@yahoo.com",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    ],
+    zip_safe=False
+)
