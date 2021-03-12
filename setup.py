53dee2b9a81dd19cb4982ed0eef406592c03c5a9
Andrew Hah <ahah@figure.com>
2021-03-12 14:36:08 -0600

    add reqs

--- setup.py
+++ setup.py
@@ -10,6 +10,10 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
+        "pandas>=1.2.2",
+        "numpy>=1.19.2",
+        "statsmodels>=0.12.2",
+        "scikit_learn>=0.24.1"
     ],
     zip_safe=False
 )
