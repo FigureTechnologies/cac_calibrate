c0532acd05a5ad82fa4cd137f764c6feedb14182
Andrew Hah <ahah@figure.com>
2021-03-22 13:03:29 -0500

    remove version reqs

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        "pandas>=1.2.2",
-        "numpy>=1.19.2",
-        "statsmodels>=0.12.2",
-        "scikit_learn>=0.24.1"
+        "pandas",
+        "numpy",
+        "statsmodels",
+        "scikit_learn"
     ],
     zip_safe=False
 )
